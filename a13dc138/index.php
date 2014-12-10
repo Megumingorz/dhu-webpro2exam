306e3c1b5b1292b1af35918a24552657f675aa46
Megming <mg.cherry@gmail.com>
2014-12-10 14:58:50 +0900

    [add] HTML FILE
    phpの実装はまだよー

---
+++ a13dc138/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>WEBプログラミング演習2最終課題</title>
+</head>
+<body>
+
+<p><a href="./">ルーム一覧</a></p>
+
+<h1>ルーム一覧</h1>
+<p>発言したいルームを選択して下さい。</p>
+
+<ul>
+    <li>【ルーム】</li>
+</ul>
+
+</body>
+</html>
---
+++ a13dc138/room.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>WEBプログラミング演習2最終課題</title>
+</head>
+<body>
+
+<p><a href="./">ルーム一覧</a></p>
+
+<h1>【ルームの名前】</h1>
+<p>【ルームの名前】</p>
+
+<ul>
+    <li>
+        <ul>
+            <li>【コメント】</li>
+            <li>【時間】</li>
+        </ul>
+    </li>
+</ul>
+<form action="record.php" method="post">
+    <input type="text" name="comment">
+    <button type="submit">送信</button>
+</form>
+
+</body>
+</html>
